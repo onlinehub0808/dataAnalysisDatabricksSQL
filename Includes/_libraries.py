4f5c25b29c3bb1767b569b63d7ad06be6e81d9e2
onlinedev0808 <onlinedev0808@gmail.com>
2022-10-21 22:32:07 +0000

    Publishing v1.2.5

--- Includes/_libraries.py
+++ Includes/_libraries.py
@@ -21,7 +21,7 @@ __validate_libraries()
 
 # COMMAND ----------
 
-version = spark.conf.get("dbacademy.library.version", "v1.0.26")
+version = spark.conf.get("dbacademy.library.version", "v1.0.28")
 default_command = f"install --quiet --disable-pip-version-check git+https://github.com/databricks-academy/dbacademy@{version}"
 pip_command = spark.conf.get("dbacademy.library.install", default_command)
 if pip_command != default_command: print(f"WARNING: Using alternative library installation:\n| default: %pip {default_command}\n| current: %pip {pip_command}")
--- Version Info.py
+++ Version Info.py
@@ -10,8 +10,8 @@
 # MAGIC %md # Project Information
 # MAGIC 
 # MAGIC * Name: **Data Analysis with Databricks**
-# MAGIC * Version:  **1.2.4**
-# MAGIC * Built On: **Oct 21, 2022 at 17:26:15 UTC**
+# MAGIC * Version:  **1.2.5**
+# MAGIC * Built On: **Oct 21, 2022 at 22:05:40 UTC**
 
 # COMMAND ----------
 
