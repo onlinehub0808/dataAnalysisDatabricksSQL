177a74a83efbb40161f3e7291d5aac41d55c475b
onlinedev0808 <onlinedev0808@gmail.com>
2022-12-15 20:24:50 +0000

    publishing v1.2.8

--- Version Info.py
+++ Version Info.py
@@ -10,8 +10,8 @@
 # MAGIC %md # Project Information
 # MAGIC 
 # MAGIC * Name: **Data Analysis with Databricks**
-# MAGIC * Version:  **1.2.7**
-# MAGIC * Built On: **Dec 14, 2022 at 21:08:34 UTC**
+# MAGIC * Version:  **1.2.8**
+# MAGIC * Built On: **Dec 15, 2022 at 20:19:25 UTC**
 
 # COMMAND ----------
 
